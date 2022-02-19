caff7bbd9d44f7d4b3dd0221b3a430cc00b4b2c0
samarasilva18 <samarasilvacandy@gmail.com>
2022-02-19 15:48:30 +0000

    Primeiro commit

---
+++ frontend/src/componentes/Componente1.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*
+function func1() {
+    return <h1>PRIMEIRO COMPONENTE</h1>
+}
+export default func1
+
+export default () => <h1>PRIMEIRO COMPONENTE ARROW</h1>
+*/
+
+export default () => (5+7)
---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Componente1 from "./componentes/Componente1"
+
+const element = document.getElementById('root')
+
+ReactDOM.render(
+    <div>
+        <Componente1/>
+    </div>
+, element
+)
